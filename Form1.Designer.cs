8c23b1a3dab070cd1b768f0d7573f0750f85c59d
Matheus Valadão <matheusnvaladao@gmail.com>
2021-04-24 23:54:14 -0300

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace MaquinaNorma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(458, 13);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsole.Size = new System.Drawing.Size(331, 173);
+            this.txtConsole.TabIndex = 0;
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(349, 74);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(30, 27);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(349, 41);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(30, 27);
+            this.btnSoma.TabIndex = 2;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Location = new System.Drawing.Point(349, 107);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(30, 27);
+            this.btnMult.TabIndex = 3;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(54, 57);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(73, 20);
+            this.txtA.TabIndex = 4;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(157, 57);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(73, 20);
+            this.txtB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Reg. A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Reg. B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 208);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.txtConsole);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//AJUSTES
+//  {
+//      FIX MULT;
+//  }
+//
+
+namespace MaquinaNorma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtConsole.ReadOnly = true;
+
+        }
+
+        private Par Initialize(Par par)
+        {
+
+            Par reg = new Par();
+            reg.name = par.name;
+            reg.value = new int[] { 0, 0 };
+
+            while (par.value[1] != 0)
+            {
+                txtConsole.AppendText("Inicializando o registrador " + reg.name + " // valor atual: " + reg.value[1] + "\n" + Environment.NewLine);
+                Dec(par);
+                Inc(reg);
+            }
+            txtConsole.AppendText("Registrador " + reg.name + " inicializado como: " + reg.value[1] + "\n" + Environment.NewLine);
+            return reg;
+        }
+        private Par Soma(Par A, Par B)
+        {
+            if (A.value[1] > B.value[1])
+            {
+                while (!Zero(B))
+                {
+                    A = Inc(A);
+                    B = Dec(B);
+                }
+                return A;
+            }
+            else
+            {
+                while (!Zero(A))
+                {
+                    B = Inc(B);
+                    A = Dec(A);
+                }
+                return B;
+            }
+
+        }
+        private Par Sub(Par A, Par B)
+        {
+            while (!Zero(B))
+            {
+                Dec(A);
+                Dec(B);
+                if (Zero(B))
+                {
+                    A.value[0] = 1;
+                }
+            }
+            return A;
+        }
+        private Par Mult(Par A, Par B)
+        {
+            Par C = new Par();
+            Par D = new Par();
+            C.name = "C";
+            C.value = new int[] { 0, 0 };
+            D.name = "D";
+            D.value = new int[] { 0, 0 };
+
+
+            while (!Zero(A))
+            {
+                C = Inc(C);
+                A = Dec(A);
+            }
+            while (!Zero(C))
+            {
+                while (!Zero(B))
+                {
+                    A = Inc(A);
+                    D = Inc(D);
+                    B = Dec(B);
+                }
+                while (!Zero(D))
+                {
+                    B = Inc(B);
+                    D = Dec(D);
+                }
+                C = Dec(C);
+            }
+            return A;
+        }
+        private Par Inc(Par par)
+        {
+            txtConsole.AppendText("Reg" + par.name + " Inc(" + par.value[1] + "), seu valor agora é: " + (par.value[1] + 1) + "\n" + Environment.NewLine);
+            par.value[1]++;
+            return par;
+        }
+        private Par Dec(Par par)
+        {
+            txtConsole.AppendText("Reg" + par.name + " Dec(" + par.value[1] + "), seu valor agora é: " + (par.value[1] - 1) + "\n" + Environment.NewLine);
+            par.value[1]--;
+            return par;
+        }
+        private bool Zero(Par X)
+        {
+            if (X.value[1] == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            txtConsole.Text = "";
+            Par A = new Par();
+            A.name = "A";
+            A.value = new int[] { 0, Convert.ToInt32(txtA.Text) };
+            Par B = new Par();
+            B.name = "B";
+            B.value = new int[] { 0, Convert.ToInt32(txtB.Text) };
+            A = Initialize(A);
+            B = Initialize(B);
+            var result = Soma(A, B);
+            txtConsole.AppendText("-- Soma finalizada --" + "\n" + Environment.NewLine);
+            txtConsole.AppendText("Resultado: " + result.value[1] + "\n" + Environment.NewLine);
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            txtConsole.Text = "";
+            Par A = new Par();
+            A.name = "A";
+            A.value = new int[] { 0, Convert.ToInt32(txtA.Text) };
+            Par B = new Par();
+            B.name = "B";
+            B.value = new int[] { 0, Convert.ToInt32(txtB.Text) };
+            A = Initialize(A);
+            B = Initialize(B);
+            var result = Sub(A, B);
+            if (result.value[0] == 0)
+            {
+                txtConsole.AppendText("-- Subtracao finalizada --" + "\n" + Environment.NewLine);
+                txtConsole.AppendText("Resultado: -" + result.value[1] + "\n" + Environment.NewLine);
+            }
+            else
+            {
+                txtConsole.AppendText("-- Subtracao finalizada --" + "\n" + Environment.NewLine);
+                txtConsole.AppendText("Resultado: " + result.value[1] + "\n" + Environment.NewLine);
+            }
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            txtConsole.Text = "";
+            Par A = new Par();
+            A.name = "A";
+            A.value = new int[] { 0, Convert.ToInt32(txtA.Text) };
+            Par B = new Par();
+            B.name = "B";
+            B.value = new int[] { 0, Convert.ToInt32(txtB.Text) };
+            A = Initialize(A);
+            B = Initialize(B);
+            var result = Mult(A, B);
+            txtConsole.AppendText("-- Multiplicação finalizada --" + "\n" + Environment.NewLine);
+            txtConsole.AppendText("Resultado: " + result.value[1] + "\n" + Environment.NewLine);
+        }
+    }
+}
